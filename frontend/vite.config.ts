687dfe5b74fb997b722ba8b834643f07f249c6cb
mateussperess <mateuspereslopesl@outlook.com>
2026-08-28 08:28:17 -0300

    feat: ajuste na url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/central-associacao/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
